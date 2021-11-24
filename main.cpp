1f5ec553ca4c0973f57873a4f8b551e973b43684
Pierre-Hugues Husson <phh@phh.me>
2021-11-24 15:50:30 -0500

    vold: Log support for exfat/texfat FS driver names
    Change-Id: I5ae38741374b25b84595a9eae9bdda7afe2cac05

--- main.cpp
+++ main.cpp
@@ -76,6 +76,8 @@ int main(int argc, char** argv) {
                << (android::vold::IsFilesystemSupported("ext4") ? " ext4" : "")
                << (android::vold::IsFilesystemSupported("f2fs") ? " f2fs" : "")
                << (android::vold::IsFilesystemSupported("ntfs") ? " ntfs" : "")
+               << (android::vold::IsFilesystemSupported("exfat") ? " exfat" : "")
+               << (android::vold::IsFilesystemSupported("texfat") ? " texfat" : "")
                << (android::vold::IsFilesystemSupported("vfat") ? " vfat" : "");
 
     VolumeManager* vm;
